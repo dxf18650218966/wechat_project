d5676a120c47518382c2b8af64d84d36e6dcbd49
dxf <18650218966@163.com>
2021-01-10 19:07:34 +0800

    。

--- parent/common/src/main/java/com/wechat/config/RedisConfig.java
+++ parent/common/src/main/java/com/wechat/config/RedisConfig.java
@@ -1,56 +1,105 @@
 package com.wechat.config;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
+import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisPassword;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettucePoolingClientConfiguration;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
-/** redis配置
- * @Auther: dxf
- * @Date: 2020/8/17 14:58
- */
-@Configuration
-@AutoConfigureAfter(RedisAutoConfiguration.class)
-public class RedisConfig {
+import java.time.Duration;
 
+@Configuration
+/**
+ * redis配置
+ * @Author dai
+ * @Date 2020/5/2
+ */
+public class RedisConfig {
     /**
-     * 配置自定义redisTemplate
-     * @return
+     * 序列化 RedisTemplate<String, Object>
+     * @return redisTemplate
      */
     @Bean
-    RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
-
+    RedisTemplate<String, Object> redisTemplate(LettuceConnectionFactory  lettuceConnectionFactory) {
+        lettuceConnectionFactory.setShareNativeConnection(false);
         RedisTemplate<String, Object> template = new RedisTemplate<>();
-        template.setConnectionFactory(redisConnectionFactory);
+        template.setConnectionFactory(lettuceConnectionFactory);
 
         // 使用Jackson2JsonRedisSerialize 替换默认序列化(默认采用的是JDK序列化)
-        Jackson2JsonRedisSerializer serializer = new Jackson2JsonRedisSerializer(Object.class);
+        Jackson2JsonRedisSerializer<Object> serializer = new Jackson2JsonRedisSerializer<>(Object.class);
 
         ObjectMapper mapper = new ObjectMapper();
         mapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
-        mapper.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+        mapper.activateDefaultTyping(LaissezFaireSubTypeValidator.instance,
+                ObjectMapper.DefaultTyping.NON_FINAL, JsonTypeInfo.As.PROPERTY);
         serializer.setObjectMapper(mapper);
-
         template.setValueSerializer(serializer);
         //使用StringRedisSerializer来序列化和反序列化redis的key值
-        template.setKeySerializer(new StringRedisSerializer());
-        template.setHashKeySerializer(new StringRedisSerializer());
+        StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
+        //key采用String的序列化方式
+        template.setKeySerializer(stringRedisSerializer);
+        // hash的key也采用String的序列化方式
+        template.setHashKeySerializer(stringRedisSerializer);
+        // value序列化方式采用jackson
+        template.setValueSerializer(serializer);
+        // hash的value序列化方式采用jackson
         template.setHashValueSerializer(serializer);
         template.afterPropertiesSet();
         return template;
     }
 
+
+    /**
+     * 序列化 StringRedisTemplate
+     * @return stringRedisTemplate
+     */
+    @Bean
+    StringRedisTemplate stringRedisTemplate(LettuceConnectionFactory  lettuceConnectionFactory) {
+        lettuceConnectionFactory.setShareNativeConnection(false);
+        StringRedisTemplate template = new StringRedisTemplate();
+        template.setConnectionFactory(lettuceConnectionFactory);
+
+        // 使用Jackson2JsonRedisSerialize 替换默认序列化(默认采用的是JDK序列化)
+        Jackson2JsonRedisSerializer<Object> serializer = new Jackson2JsonRedisSerializer<>(Object.class);
+
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        mapper.activateDefaultTyping(LaissezFaireSubTypeValidator.instance,
+                ObjectMapper.DefaultTyping.NON_FINAL, JsonTypeInfo.As.PROPERTY);
+        serializer.setObjectMapper(mapper);
+        template.setValueSerializer(serializer);
+        // 使用StringRedisSerializer来序列化和反序列化redis的key值
+        StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
+        // key采用String的序列化方式
+        template.setKeySerializer(stringRedisSerializer);
+        // value序列化方式采用jackson
+        template.setValueSerializer(serializer);
+        template.afterPropertiesSet();
+        return template;
+    }
 }
-/**  @Autowired
- *   private RedisTemplate redisTemplate;
- *
- *  如果我们没有把哪个bean命名为redisTemplate 而这里又这么注入，会导致我们使用的springboot内置的tempalte而不是我们配置的，
- *  导致我们的配置不生效，从而埋坑，所以最好的方法就是把方法命名为redisTemplate从而覆盖系统内置的。
+
+/**
+ *      *
+ *      * 使用：
+ *      *    @Autowired
+ *      *    private RedisTemplate redisTemplate;
+ *      *    如果我们没有把哪个bean命名为redisTemplate 而这里又这么注入，会导致我们使用的springboot内置的tempalte而不是我们配置的，
+ *      *    导致我们的配置不生效，从而埋坑，所以最好的方法就是把方法命名为redisTemplate从而覆盖系统内置的。
  */
